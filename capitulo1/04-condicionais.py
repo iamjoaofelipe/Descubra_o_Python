a1c9e8ae79b444cab9d60687a7f01e3eb9ba692a
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2023-01-28 13:45:20 -0300

    Estruturas condicionais

--- capitulo1/04-condicionais.py
+++ capitulo1/04-condicionais.py
@@ -3,13 +3,13 @@
 #
 
 def condicionais():
-    x , y = 1 , 1
+    x , y = 1000 , 1000
     if (x > y):
         print("X é maior que Y")
-    elif(x == y):
-        print("O valor de X e Y são iguais")    
-    if(y > x):
-        print("Y é maior que X")
+    elif(x < y):
+        print("Y é maior que X")    
+    if(x == y):
+        print("O valor de X e Y são iguais")
             
 condicionais()
 

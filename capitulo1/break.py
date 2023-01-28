6367903e31e36a757117feecd02c79dcf1e5e3dd
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2023-01-28 15:53:39 -0300

    Parando ou continuando um loop

---
+++ capitulo1/break.py
@@ -0,0 +1,20 @@
+#
+# Exemplo de como usar os comando Break e Continue
+#
+
+def loopBreak():
+    for x in range(5,10):
+        if(x == 7):
+            break
+        print("O valor do x é:" , x)
+loopBreak()
+
+
+def loopBreak():
+    for c in range(5,10):
+        if(c == 7):
+            continue
+        print("O valor do x é:" , c)
+loopBreak()
+
+

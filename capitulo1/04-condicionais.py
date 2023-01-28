323a7b6b4ee02147b60ceada05eade4bef28f4a9
João Felipe Dos Santos Barreto <79280942+iamjoaofelipe@users.noreply.github.com>
2023-01-28 13:19:32 -0300

    Estruturas condicionais

---
+++ capitulo1/04-condicionais.py
@@ -0,0 +1,15 @@
+#
+# Arquivo de exemplo das estruturas condicionais
+#
+
+def condicionais():
+    x , y = 1 , 1
+    if (x > y):
+        print("X é maior que Y")
+    elif(x == y):
+        print("O valor de X e Y são iguais")    
+    if(y > x):
+        print("Y é maior que X")
+            
+condicionais()
+
